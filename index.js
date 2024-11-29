db44ffb6ef462e5a38e67bd9bd39b3577c4f7b63
fredy <fredy-andri.mottus@voco.ee>
2024-11-29 11:55:07 +0200

    kuni ül 3. keskeni lahendused

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
+
+const key = 'c17b46fb8644cc284b164e07909bbdbb'
+let city ='Tartu'
+
+app.get('/', (req, res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    res.render('index')
+})
+
+app.listen(3002)
+
+//pooleni jäi 3. keskel//
